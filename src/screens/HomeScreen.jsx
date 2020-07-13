59827fae5e5b18cc007b4c35c6ff77d075639b29
岡本透青 <okamototooruao@okamototooruaonoMacBook-Pro.local>
2020-07-14 01:49:23 +0900

    SPAの作成練習

---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
+
+const HomeScreen = ({navigation}) => {
+  
+  return(
+    <View>
+      <Text style={styles.text}>Hi there!</Text>
+      <Button 
+        title='Go to Components Demo' 
+        onPress={() => navigation.navigate('Components')}
+      />
+      <Button 
+        title='Go to List Demo'
+        onPress={() => navigation.navigate('List')}
+      />
+      <Button 
+        title='Go to Image Demo'
+        onPress={() => navigation.navigate('Image')}
+      />
+      <TouchableOpacity onPress={() => navigation.navigate('List') }>
+        <Text>Go to List Demo</Text>
+        <Text>Go to List Demo</Text>
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30
+  }
+});
+
+export default HomeScreen;
